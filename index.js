4f797c1abbefb28748442b47b293c23717bee8f8
Graham <gk@Grahams-iMac.local>
2018-01-14 17:33:37 +0000

    babel transpile own code if it is within node_modules

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
-require('babel-register');
+require('babel-register')({
+  ignore: /node_modules[\\/](?!@alphaeadev\/node-cf-deploy)/
+});
 require('babel-polyfill');
 require('./src/main.js');
